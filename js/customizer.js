db86d2e0610c4a50b086d9249586c6ce8f1a7939
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-07-21 11:39:40 +0300

    Fix Site Title and Tagline Customizer settings.

--- js/customizer.js
+++ js/customizer.js
@@ -8,33 +8,29 @@
 	// Site title and description.
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
-			$( '.site-title' ).text( to );
+			$( '.home-title a' ).text( to );
 		} );
 	} );
 	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
+			$( '.home-description' ).text( to );
 		} );
 	} );
 	// Header text color.
 	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
+				$( '.home-title a, .home-description' ).css( {
 					'clip': 'rect(1px, 1px, 1px, 1px)',
 					'position': 'absolute'
 				} );
 			} else {
-				$( '.site-title, .site-description' ).css( {
+				$( '.home-title a, .home-description' ).css( {
 					'clip': 'auto',
-					'position': 'static'
+					'position': 'relative'
 				} );
-
-				$( '.site-title a, .site-description' ).css( {
+				$( '.home-title a' ).css( {
 					'color': to
-				} );
-				$( '.site-title a' ).css( {
-					'border-color': to
 				} );
 			}
 		} );
